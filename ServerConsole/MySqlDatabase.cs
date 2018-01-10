70c6b38156bffe85d74041dc5e64b9048662705d
MaciejRozmiarek <maciej.rozmiarek@gmail.com>
2018-01-10 23:31:40 +0100

    Pierwszy

---
+++ ServerConsole/MySqlDatabase.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace ServerConsole
+{
+    class MySqlDatabase
+    {
+        public MySqlConnection connection;
+
+        string connectionString = "SERVER=localhost;DATABASE=mysql;UID=root;PASSWORD=;";
+
+        public bool OpenConnection()
+        {
+            connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                return true;
+
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        public bool insertToDB(string Query)
+        {
+            string query = Query;
+            if (this.OpenConnection() == true)
+            {
+                try
+                {
+                    //create command and assign the query and connection from the constructor
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    //Execute command
+
+                    //dodac try cach dla duplikatow i bledow
+                    cmd.ExecuteNonQuery();
+
+                    this.CloseConnection();
+
+                    return true;
+                }
+                catch (Exception exc)
+                {
+
+                    return false;
+                }
+
+
+                //close connection
+
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public DataSet getDataSet(string Command)
+        {
+            string sqlCmd = Command;
+            DataSet DS = new DataSet();
+            if (this.OpenConnection() == true)
+            {
+
+                MySqlCommand cmd = new MySqlCommand(sqlCmd, connection);
+                cmd.CommandType = CommandType.Text;
+                // MySqlDataReader rdr = cmd.ExecuteReader();
+
+                MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(sqlCmd, connection);
+
+                mySqlDataAdapter.Fill(DS);
+                mySqlDataAdapter.Dispose();
+
+            }
+            return DS;
+
+        }
+        public int checkLogin(string Szukana)
+        {
+            string szukana = Szukana;
+            int Count = -1;
+            string sqlCmd = "select count(*) from uzytkownicy where login = '" + szukana + "';";
+
+            if (this.OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(sqlCmd, connection);
+
+                //ExecuteScalar will return one value
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                this.CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+
+        }
+    }
+}
---
+++ ServerConsole/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            MySqlDatabase conn = new MySqlDatabase();
+            try
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                // Set the TcpListener on port 13000.
+                Int32 port = 5000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                conn.OpenConnection();
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        string query = System.Text.Encoding.UTF8.GetString(bytes, 0, i);
+                        Console.WriteLine("Received query: {0}", query);
+
+                        // Process the data sent by the client.
+                        var ds = conn.getDataSet(query);
+
+                        // Send back a response.
+                        binaryFormatter.Serialize(stream, ds);
+
+                        Console.WriteLine("Sent.");
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+
+                conn.CloseConnection();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
